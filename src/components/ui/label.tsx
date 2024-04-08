59df71df471360058b85a3a814f24797e790269a
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-04-08 18:00:56 -0300

    bug resolvido

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const labelVariants = cva(
-  'tailwind.config.jstext-sm tailwind.config.jsfont-medium tailwind.config.jsleading-none peer-disabled:tailwind.config.jscursor-not-allowed peer-disabled:tailwind.config.jsopacity-70',
+  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
 )
 
 const Label = React.forwardRef<
