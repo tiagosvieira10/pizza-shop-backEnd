3348f720aa1e7c4e3286b863b9af3fae68518e26
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-04-08 16:15:27 -0300

    helmet add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { RouterProvider } from 'react-router-dom'
 import './global.css'
-import { Button } from './components/ui/button'
+import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
