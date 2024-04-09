c78041ee7b86c40f4b17087afbb6616c3d6a3a7d
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-04-09 15:23:48 -0300

    sign-up finalizada

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from './pages/app/dashboard'
 import { AppLayout } from './pages/_layouts/app'
 import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
 ])
