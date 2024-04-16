3ced9cf93325572d2cf8638e695e23dd960c0ffc
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-04-16 09:53:16 -0300

    interface otimista

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -6,5 +6,7 @@ interface UpdateProfileBody {
 }
 
 export async function updateProfile({ name, description }: UpdateProfileBody) {
+  throw new Error()
+
   await api.put('/profile', { name, description })
 }
