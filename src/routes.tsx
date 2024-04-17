cbc01beaf558708ca6f6e1733253590cf324ff00
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-04-17 14:17:08 -0300

    filtros dos perdidos- bug

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
-import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './pages/_layouts/auth'
-import { SignUp } from './pages/auth/sign-up'
-import { Orders } from './pages/app/orders/orders'
-import { Dashboard } from './pages/app/dashboard/dashboard'
 import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/orders/orders'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -14,17 +14,28 @@ export const router = createBrowserRouter([
     element: <AppLayout />,
     errorElement: <NotFound />,
     children: [
-      { path: '/', element: <Dashboard /> },
-      { path: '/orders', element: <Orders /> },
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
     ],
   },
-
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      { path: '/sign-in', element: <SignIn /> },
-      { path: '/sign-up', element: <SignUp /> },
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
     ],
   },
 ])
